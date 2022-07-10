d398f36b96c3972994b54e39005e151d1024c067
si-shakil <sishakil850@gmail.com>
2022-07-10 15:47:52 +0600

    payment page css added

--- src/Components/CheckOutPage/PaymentCheckOut.js
+++ src/Components/CheckOutPage/PaymentCheckOut.js
@@ -3,41 +3,45 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../firebase.init";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import CheckOutItem from "./CheckOutItem";
+import { Link } from "react-router-dom";
 
 function PaymentCheckOut() {
   const [user] = useAuthState(auth);
   const { cartItems } = useSelector((store) => store.cart);
   return (
-    <div className="payment">
+    <div className="bg-white ">
       <div className="container">
+        <h1 className="text-center p-2 font-[400] bg-[#ece3e3] border-b-[2px] border-b-[lightgray]">
+          Checkout (<Link to="/checkout">{cartItems?.length} items</Link>)
+        </h1>
         {/* delivery address */}
-        <div className="section">
-          <div className="title">
+        <div className="flex p-5 my-0 mx-5 border-b-[1px] border-b-[lightgray]">
+          <div className="flex-[0.2]">
             <h1 className="text-xl font-medium">Delivery Address</h1>
           </div>
-          <div className="address">
+          <div className="flex-[0.8] ml-9">
             <p>{user?.displayName}</p>
             <p>{user?.email}</p>
             <p>Los Angeles, CA</p>
           </div>
         </div>
         {/* review items */}
-        <div className="section">
-          <div className="title">
+        <div className="flex p-5 my-0 mx-5 border-b-[1px] border-b-[lightgray]">
+          <div className="flex-[0.2]">
             <h1 className="text-xl font-medium">Review items and delivery</h1>
           </div>
-          <div className="items">
+          <div className="flex-[0.8]">
             {cartItems.map((item) => (
               <CheckOutItem key={item.id} item={item} />
             ))}
           </div>
         </div>
         {/* payment method */}
-        <div className="section">
-          <div className="title">
+        <div className="flex p-5 my-0 mx-5 border-b-[1px] border-b-[lightgray]">
+          <div className="flex-[0.2]">
             <h1 className="text-xl font-medium">Payment Method</h1>
           </div>
-          <div className="details">{/* stripe */}</div>
+          <div className="flex-[0.8]">{/* stripe */}</div>
         </div>
       </div>
     </div>
